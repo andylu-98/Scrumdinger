6f263f888e97138fd9c1ef06300440419d3ea4a3
Andy <fantasticandycandy@gmail.com>
2025-06-25 23:56:04 +0100

    project creation

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Andy Lu on 25/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
